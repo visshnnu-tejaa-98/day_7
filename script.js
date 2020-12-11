3390286711e95500cf239b0f966d44585495322a
visshnnu-tejaa-98 <74036897+visshnnu-tejaa-98@users.noreply.github.com>
2020-12-11 10:30:24 +0530

    code modified

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
-let request = new XMLHttpRequest();
+var request  = new XMLHttpRequest()
 request.open("GET","https://restcountries.eu/rest/v2/all",true)
-request.send();
+request.send()
 request.onload = function(){
     let data = JSON.parse(this.response)
     let country = data.filter(country=>country.name==="India")
-    let latitude = country[0].latlng[0];
-    let longitude = country[0].latlng[1];
-    let key = "7e01312693145aa48718efca21b9550f"
-    let req = new XMLHttpRequest();
-    let url = "http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid="+key
-    req.open("GET",url,true)
-    req.send();
-    req.onload = function(){
-        let info = JSON.parse(this.response)
-        console.log(info)
-    }
-
-    let request = new XMLHttpRequest()
-    let cityName = "Germany"
-    request.open("GET","http://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=7e01312693145aa48718efca21b9550f",true)
-    request.send();
-    request.onload = function(){
-        let info = JSON.parse(this.response)
-        console.log(info)
-    }
-
+    let lat = country[0].latlng[0];
+    let long = country[0].latlng[1];
+    let cityName = "Germany";
+    getWeatherData(lat,long);
+    getWeather(cityName)
 }
+
+function getWeatherData(lat,long){
+    var request = new XMLHttpRequest();
+    let url = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&appid=7e01312693145aa48718efca21b9550f"
+    request.open("GET",url,true);
+    request.send()
+    request.onload = function(){
+        var data = JSON.parse(this.response)
+        console.log(data)
+    }
+}   
+
+function getWeather(cityName){
+    var request = new XMLHttpRequest();
+    let url = "http://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=7e01312693145aa48718efca21b9550f"
+    request.open("GET",url,true);
+    request.send()
+    request.onload = function(){
+        var data = JSON.parse(this.response)
+        console.log(data)
+    }
+}
+
 
